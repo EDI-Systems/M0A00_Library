6877046235c1a255ab9f6065376b69ed2eec2e15
Runyu Pan <rypan@sdu.edu.cn>
2024-06-05 21:33:17 +0800

    :arrow_up: add DSP281x library

---
+++ DSP281x_Library/DSP281x_common/source/DSP281x_XIntrupt.c
@@ -0,0 +1,29 @@
+// TI File $Revision: /main/2 $
+// Checkin $Date: April 29, 2005   11:00:54 $
+//###########################################################################
+//
+// FILE:   DSP281x_XIntrupt.c
+//
+// TITLE:  DSP28 External Interrupt Initialization & Support Functions.
+//
+//###########################################################################
+// $TI Release: DSP281x C/C++ Header Files V1.20 $
+// $Release Date: July 27, 2009 $
+//###########################################################################
+
+#include "DSP281x_Device.h"     // DSP281x Headerfile Include File
+#include "DSP281x_Examples.h"   // DSP281x Examples Include File
+
+//---------------------------------------------------------------------------
+// InitXIntrupt: 
+//---------------------------------------------------------------------------
+// This function initializes external interrupts to a known state.
+//
+void InitXIntrupt(void)
+{
+
+}
+
+//===========================================================================
+// No more.
+//===========================================================================
